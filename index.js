f3c2aec5a64f5043d5caecd21e934c1012622b66
KA <kjangulo@gmail.com>
2025-06-05 19:18:46 -0400

    Setting up dotenv and authorization

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// Load environment variables from .env file
+const dotenv = require('dotenv');
+dotenv.config({path: '.env'});
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
